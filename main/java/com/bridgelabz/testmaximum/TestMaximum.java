b129700fc8998d0cbe7f6a781589bcdd9c2a41bb
sharale34 <sourabhharale8000@gmail.com>
2020-10-09 10:18:34 +0530

    Added: To find maximum of 3 floats given along with test cases is added

--- main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -4,8 +4,9 @@ public class TestMaximum {
 
 	public static void main(String[] args) {
 		Integer x = 3, y = 4, z = 10;
-		System.out.println("Welcome to the Test Maximum Program");
+		Float x1 = 10.55f, y1 = 29.22f, z1 = 23.2f;
 		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumInt(x, y, z));
+		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumFloat(x1, y1, z1));
 	}
 
 	public static Integer testMaximumInt(Integer x, Integer y, Integer z) {
@@ -15,6 +16,17 @@ public class TestMaximum {
 		}
 		if (z.compareTo(max) > 0) {
 			max = z;
+		}
+		return max;
+	}
+
+	public static Float testMaximumFloat(Float x1, Float y1, Float z1) {
+		Float max = x1;
+		if (y1.compareTo(max) > 0) {
+			max = y1;
+		}
+		if (z1.compareTo(max) > 0) {
+			max = z1;
 		}
 		return max;
 	}
---
+++ test/java/com/bridgelabz/testmaximum/TestMaximumFloat.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.testmaximum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMaximumFloat {
+	@Test
+	public void testFirstMaxInteger() {
+		Float max = TestMaximum.testMaximumFloat(30.5f, 10.25f, 20.65f);
+		boolean firstFloat;
+		if (max.equals(30.5f)) {
+			firstFloat = true;
+		} else {
+			firstFloat = false;
+		}
+		Assert.assertEquals(true, firstFloat);
+	}
+
+	@Test
+	public void testSecondMaxFloat() {
+		Float max = TestMaximum.testMaximumFloat(10.25f, 30.5f, 20.65f);
+		boolean secondFloat;
+		if (max.equals(30.5f)) {
+			secondFloat = true;
+		} else {
+			secondFloat = false;
+		}
+		Assert.assertEquals(true, secondFloat);
+	}
+	
+	@Test
+	public void testThirdMaxFloat() {
+		Float max = TestMaximum.testMaximumFloat(10.25f, 30.5f, 40.65f);
+		boolean thirdFloat;
+		if (max.equals(40.65f)) {
+			thirdFloat = true;
+		} else {
+			thirdFloat = false;
+		}
+		Assert.assertEquals(true, thirdFloat);
+	}
+}

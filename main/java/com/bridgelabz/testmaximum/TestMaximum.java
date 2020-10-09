89f961b740d0314a715099e2ae208e475d3a4808
sharale34 <sourabhharale8000@gmail.com>
2020-10-09 11:15:32 +0530

    Added:  To find maximum of 3 strings given along with test cases is added

--- main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -5,8 +5,10 @@ public class TestMaximum {
 	public static void main(String[] args) {
 		Integer x = 3, y = 4, z = 10;
 		Float x1 = 10.55f, y1 = 29.22f, z1 = 23.2f;
+		String x2 = "Apple", y2 = "Peach", z2 = "Banana";
 		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumInt(x, y, z));
 		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumFloat(x1, y1, z1));
+		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumString(x2, y2, z2));
 	}
 
 	public static Integer testMaximumInt(Integer x, Integer y, Integer z) {
@@ -30,4 +32,16 @@ public class TestMaximum {
 		}
 		return max;
 	}
+
+	public static String testMaximumString(String x2, String y2, String z2) {
+		String max = x2;
+		if (y2.compareTo(max) > 0) {
+			max = y2;
+		}
+		if (z2.compareTo(max) > 0) {
+			max = z2;
+		}
+		return max;
+	}
+
 }
---
+++ test/java/com/bridgelabz/testmaximum/TestMaximumString.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.testmaximum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMaximumString {
+	@Test
+	public void testFirstMaxString() {
+		String max = TestMaximum.testMaximumString("Peach", "Apple", "Banana");
+		boolean firstString;
+		if (max.equals("Peach")) {
+			firstString = true;
+		} else {
+			firstString = false;
+		}
+		Assert.assertEquals(true, firstString);
+	}
+
+	@Test
+	public void testSecondMaxString() {
+		String max = TestMaximum.testMaximumString("Apple", "Peach", "Banana");
+		boolean secongString;
+		if (max.equals("Peach")) {
+			secongString = true;
+		} else {
+			secongString = false;
+		}
+		Assert.assertEquals(true, secongString);
+	}
+
+	@Test
+	public void testThirdMaxString() {
+		String max = TestMaximum.testMaximumString("Apple", "Banana", "Peach");
+		boolean thirdString;
+		if (max.equals("Peach")) {
+			thirdString = true;
+		} else {
+			thirdString = false;
+		}
+		Assert.assertEquals(true, thirdString);
+	}
+}

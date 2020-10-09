1d1b46900edbda32881adc05e1acb24220392ddf
sharale34 <sourabhharale8000@gmail.com>
2020-10-09 09:51:40 +0530

    Added: To find maximum of 3 integers given along with test cases is added

--- main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -3,6 +3,19 @@ package com.bridgelabz.testmaximum;
 public class TestMaximum {
 
 	public static void main(String[] args) {
+		Integer x = 3, y = 4, z = 10;
 		System.out.println("Welcome to the Test Maximum Program");
+		System.out.println("Maximum of three integers given is : " + TestMaximum.testMaximumInt(x, y, z));
+	}
+
+	public static Integer testMaximumInt(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
 	}
 }
---
+++ test/java/com/bridgelabz/testmaximum/TestMaximumInteger.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.testmaximum;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class TestMaximumInteger {
+	@Test
+	public void testFirstMaxInteger() {
+		Integer max = TestMaximum.testMaximumInt(30, 10, 20);
+		boolean firstInteger;
+		if (max.equals(30)) {
+			firstInteger = true;
+		} else {
+			firstInteger = false;
+		}
+		Assert.assertEquals(true, firstInteger);
+	}
+
+	@Test
+	public void testSecondMaxInteger() {
+		Integer max = TestMaximum.testMaximumInt(20, 30, 10);
+		boolean secondInteger;
+		if (max.equals(30)) {
+			secondInteger = true;
+		} else {
+			secondInteger = false;
+		}
+		Assert.assertEquals(true, secondInteger);
+	}
+
+	@Test
+	public void testThirdMaxInteger() {
+		Integer max = TestMaximum.testMaximumInt(20, 30, 40);
+		boolean thirdInteger;
+		if (max.equals(40)) {
+			thirdInteger = true;
+		} else {
+			thirdInteger = false;
+		}
+		Assert.assertEquals(true, thirdInteger);
+	}
+}

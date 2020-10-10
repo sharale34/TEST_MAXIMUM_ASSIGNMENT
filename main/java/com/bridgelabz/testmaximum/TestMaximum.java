fd057a402a736d3692b57ebffddee567fb7389a4
sharale34 <sourabhharale8000@gmail.com>
2020-10-10 10:15:55 +0530

    Modified: Extended the method to take more than three parameters

--- main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -1,38 +1,20 @@
 package com.bridgelabz.testmaximum;
 
+import java.util.Arrays;
+
 public class TestMaximum<T extends Comparable<T>> {
-	T x, y, z;
-
-	public TestMaximum(T x, T y, T z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-
-	public T testMaximum() {
-		return TestMaximum.testMaximum(x, y, z);
-	}
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to the test maximum program");
-		TestMaximum.testMaximum(10, 56, 26);
+		TestMaximum.testMaximum(10, 56, 26, 102, 78);
 		TestMaximum.testMaximum(8.5f, 4.5f, 5.5f);
-		TestMaximum.testMaximum("Apple", "Peach", "Banana");
+		TestMaximum.testMaximum("Apple", "Peach", "Banana", "Custard");
 	}
 
-	public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
-		T max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		printMax(x, y, z, max);
+	public static <T extends Comparable<T>> T testMaximum(T... values) {
+		Arrays.sort(values);
+		T max = values[values.length - 1];
+		System.out.printf("The maximum is: %s.\n", max);
 		return max;
 	}
 
-	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("The maximum of %s, %s and %s is: %s.\n", x, y, z, max);
-	}
 }

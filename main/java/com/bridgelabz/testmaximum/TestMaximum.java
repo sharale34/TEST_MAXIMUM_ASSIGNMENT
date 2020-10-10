41c4da07cb871289430f4a63656b90f80a7a1ac2
sharale34 <sourabhharale8000@gmail.com>
2020-10-10 10:24:53 +0530

    Added: Extended the method to print using generic method

--- main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -3,18 +3,22 @@ package com.bridgelabz.testmaximum;
 import java.util.Arrays;
 
 public class TestMaximum<T extends Comparable<T>> {
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the test maximum program");
-		TestMaximum.testMaximum(10, 56, 26, 102, 78);
+		TestMaximum.testMaximum(10, 56, 26);
 		TestMaximum.testMaximum(8.5f, 4.5f, 5.5f);
-		TestMaximum.testMaximum("Apple", "Peach", "Banana", "Custard");
+		TestMaximum.testMaximum("Apple", "Peach", "Banana");
 	}
 
 	public static <T extends Comparable<T>> T testMaximum(T... values) {
 		Arrays.sort(values);
 		T max = values[values.length - 1];
-		System.out.printf("The maximum is: %s.\n", max);
+		printMaxParameter(max);
 		return max;
 	}
 
+	public static <Parameter> void printMaxParameter(Parameter max) {
+		System.out.printf("The maximum is: %s.\n", max);
+	}
 }
